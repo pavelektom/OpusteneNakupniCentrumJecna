f6fb7f62199dc71f397bc6be85af74d4b16d898d
tompa <tompavelek23@gmail.com>
2026-01-23 10:35:16 +0100

    dodelan konstruktor na predmety, gettery a settery

--- src/KonzoleVeci/Predmet.java
+++ src/KonzoleVeci/Predmet.java
@@ -6,4 +6,42 @@ public class Predmet {
     private boolean prenosny;
     private boolean skryty;
 
+    public Predmet(String nazev, String popis, boolean prenosny, boolean skryty) {
+        this.nazev = nazev;
+        this.popis = popis;
+        this.prenosny = prenosny;
+        this.skryty = skryty;
+    }
+
+    public String getNazev() {
+        return nazev;
+    }
+
+    public void setNazev(String nazev) {
+        this.nazev = nazev;
+    }
+
+    public String getPopis() {
+        return popis;
+    }
+
+    public void setPopis(String popis) {
+        this.popis = popis;
+    }
+
+    public boolean isPrenosny() {
+        return prenosny;
+    }
+
+    public void setPrenosny(boolean prenosny) {
+        this.prenosny = prenosny;
+    }
+
+    public boolean isSkryty() {
+        return skryty;
+    }
+
+    public void setSkryty(boolean skryty) {
+        this.skryty = skryty;
+    }
 }
