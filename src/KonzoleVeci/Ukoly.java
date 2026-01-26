836e534dd159199fb7553f5d7352a99df29e9272
tompa <tompavelek23@gmail.com>
2026-01-26 09:33:23 +0100

    zaklady ukoly

--- src/KonzoleVeci/Ukoly.java
+++ src/KonzoleVeci/Ukoly.java
@@ -3,9 +3,18 @@ package KonzoleVeci;
 public class Ukoly {
     private String nazev;
     private String popis;
+    private boolean splneny;
 
     public String getNazev() {
         return nazev;
+    }
+
+    public boolean isSplneny() {
+        return splneny;
+    }
+
+    public void setSplneny(boolean splneny) {
+        this.splneny = true;
     }
 
     public void setNazev(String nazev) {
