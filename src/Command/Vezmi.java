05b9f1155cf429acd3eec0f9bf543af585811559
tompa <tompavelek23@gmail.com>
2026-01-28 10:36:23 +0100

    opraveni prikazu vezmi

--- src/Command/Vezmi.java
+++ src/Command/Vezmi.java
@@ -1,11 +1,27 @@
 package Command;
 
 import KonzoleVeci.Hra;
+import KonzoleVeci.Inventory;
+import KonzoleVeci.Mistnost;
+import KonzoleVeci.Predmet;
 
 public class Vezmi implements Command {
 
     @Override
     public void vykonat(Hra hra, String s) {
+        if (s.trim().equals("")){
+            System.out.println("Co chces vzit? ");
+        } else {
+           Inventory inventar = hra.getInventar();
+           Predmet p = hra.getNacitani().najdiPredmet(s);
+           inventar.pridej(p);
+           hra.getAktualniMistnost().odeberPredmet(s);
+           System.out.println("Predmet byl pridan do inventare ");
+        }
+
+
+
 
     }
 }
+
