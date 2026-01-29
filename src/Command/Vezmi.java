dc54a78a96f72fd2f11764f74238cc8d89fa555d
tompa <tompavelek23@gmail.com>
2026-01-29 15:24:18 +0100

    vezmi uz funkcni dokonale

--- src/Command/Vezmi.java
+++ src/Command/Vezmi.java
@@ -7,17 +7,34 @@ import KonzoleVeci.Predmet;
 
 public class Vezmi implements Command {
 
+
+
     @Override
     public String vykonat(Hra hra, String s) {
         if (s.trim().equals("")) {
-            System.out.println("Co chces vzit? ");
+            return "Co chces vzit? ";
         }
-       Inventory inventar = hra.getInventar();
-       Predmet p = hra.getNacitani().najdiPredmet(s);
-       inventar.pridej(p);
-       hra.getAktualniMistnost().odeberPredmet(s);
-       return "Predmet byl pridan do inventare ";
+        String nazev = s.trim();
+        Predmet p = hra.getAktualniMistnost().odeberPredmet(nazev);
 
+        if (p == null) {
+            return "Takovy predmet tu neni..";
+        }
+        if (p.isSkryty()) {
+            hra.getAktualniMistnost().pridatPredmet(p);
+            return "Tenhle predmet tu mozna je, kdo vi...";
+        }
+        if (!p.isHledan()) {
+            hra.getAktualniMistnost().pridatPredmet(p);
+            return "Tenhle predmet tu mooooznaaaa je, zkus treba jeste neco...";
+        }
+
+        Inventory inventar = hra.getInventar();
+        if (!inventar.pridej(p)) {
+            hra.getAktualniMistnost().pridatPredmet(p);
+            return "Inventar je plny neco musis polozit jestli chces toto zvednout :(";
+        }
+        return "Vzal jsi: " + p.getNazev();
     }
 }
 
