44590109dff42cdb0cb0559f30a01b6714a1d29f
tompa <tompavelek23@gmail.com>
2026-01-29 21:29:38 +0100

    postava dodelany dulezity veci

--- src/Postavy/Postava.java
+++ src/Postavy/Postava.java
@@ -2,7 +2,18 @@ package Postavy;
 
 public class Postava {
     private String jmeno;
+    private String popis;
     private String dialog;
+    
+
+
+    public String getPopis() {
+        return popis;
+    }
+
+    public void setPopis(String popis) {
+        this.popis = popis;
+    }
 
     public String getJmeno() {
         return jmeno;
