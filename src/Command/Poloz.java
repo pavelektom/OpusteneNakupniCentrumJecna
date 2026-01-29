be82045ffa8d74bc6b800da7bd869866394f1f15
tompa <tompavelek23@gmail.com>
2026-01-29 21:28:22 +0100

    poloz hotovy

--- src/Command/Poloz.java
+++ src/Command/Poloz.java
@@ -9,17 +9,19 @@ public class Poloz implements Command {
 
     @Override
     public String vykonat(Hra hra, String s) {
+
         String ahoj = s.trim().toLowerCase();
         if (ahoj.equals("") || s.trim().isEmpty() == true) {
             return "Vyberte jaky predmet chcete odebrat z inventare..";
         }
         Inventory inventar = hra.getInventar();
         Predmet p = inventar.odeber(ahoj);
-        hra.getAktualniMistnost().pridatPredmet(p);
-
         if (p == null){
             return "Takovy predmet nemas";
         }
+        hra.getAktualniMistnost().pridatPredmet(p);
+
+
 
         return "Predmet " + s + " byl polozen.";
     }
