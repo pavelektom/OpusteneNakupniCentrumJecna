fcc1f1e4011cf416be743cf41a7480ae9583293f
tompa <tompavelek23@gmail.com>
2026-01-29 21:30:16 +0100

    predmety extremne hotovy

--- src/KonzoleVeci/Predmet.java
+++ src/KonzoleVeci/Predmet.java
@@ -4,11 +4,24 @@ public class Predmet {
     private String nazev;
     private String popis;
     private boolean skryty;
+    private boolean hledan;
+
+    public boolean isHledan() {
+        return hledan;
+    }
+
+    public void setHledan(boolean hledan) {
+        this.hledan = hledan;
+    }
 
     public Predmet(String nazev, String popis, boolean skryty) {
         this.nazev = nazev;
         this.popis = popis;
         this.skryty = skryty;
+    }
+
+    public void odkryt(){
+        this.skryty = false;
     }
 
     public String getNazev() {
@@ -29,6 +42,10 @@ public class Predmet {
 
     public boolean isSkryty() {
         return skryty;
+    }
+    @Override
+    public String toString() {
+        return nazev +" - " + popis;
     }
 
     public void setSkryty(boolean skryty) {
