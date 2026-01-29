9b2b483441579689535ace050a7c6c70637a9b47
tompa <tompavelek23@gmail.com>
2026-01-29 15:36:25 +0100

    hotove a funkcni hledat

--- src/Command/Hledat.java
+++ src/Command/Hledat.java
@@ -1,11 +1,35 @@
 package Command;
 
 import KonzoleVeci.Hra;
+import KonzoleVeci.Mistnost;
+import KonzoleVeci.Predmet;
 
 public class Hledat implements Command {
 
     @Override
     public String vykonat(Hra hra, String s) {
-        return null;
+        Mistnost m =hra.getAktualniMistnost();
+        String vypis = "";
+        boolean ahoj = true;
+        boolean neco = false;
+
+        for (Predmet p : m.getPredmety()) {
+            if (!p.isSkryty() && !p.isHledan()) {
+                p.setHledan(true);
+                neco = true;
+                if (!ahoj) {
+                    vypis += "";
+                }
+                vypis += p.getNazev();
+                ahoj = false;
+            }
+        }
+
+        if (!neco) {
+            return "Nic dalsiho jsi nenasel.";
+        }
+
+        return "Nasel jsi: " + vypis;
+
     }
 }
