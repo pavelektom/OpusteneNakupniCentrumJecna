ec648542aa15bcfb865f668e51e200ea30b214ef
tompa <tompavelek23@gmail.com>
2026-01-29 21:44:26 +0100

    pridani kdo je v mistnosti do napovedy

--- src/Command/Napoveda.java
+++ src/Command/Napoveda.java
@@ -1,6 +1,7 @@
 package Command;
 
 import KonzoleVeci.Hra;
+import Postavy.Postava;
 
 import java.util.Scanner;
 
@@ -11,13 +12,21 @@ public class Napoveda implements Command{
     public String vykonat(Hra hra, String s) {
         System.out.println("S cim potrebujes pomoct?");
         System.out.println("1 - Kam mam jit?");
-        System.out.println("2 - ");
+        System.out.println("2 - Kdo je v teto mistnosti");
         String emm = ":D";
         switch (sc.next()) {
             case "1":
-                System.out.println(hra.getAktualniMistnost().getSousedi());
+                System.out.println("Dostupne vychody - ");
+                System.out.print(hra.getAktualniMistnost().getSousedi());
             case "2":
-
+                if (hra.getAktualniMistnost().getPostavy().isEmpty()) {
+                    return "V mistnosti nikdo neni.";
+                }
+                String text = "V mistnosti jsou postavy: ";
+                for (Postava p : hra.getAktualniMistnost().getPostavy()) {
+                    text = text + "\n" + " - " + p.getJmeno();
+                }
+                return text;
         }
         return emm;
     }
