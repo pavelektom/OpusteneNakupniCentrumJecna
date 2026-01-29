f51c891910799334e10fa82c58351fdd46cf7f12
tompa <tompavelek23@gmail.com>
2026-01-29 15:37:37 +0100

    hotove a funkcni prozkoumej

--- src/Command/Prozkoumej.java
+++ src/Command/Prozkoumej.java
@@ -1,11 +1,31 @@
 package Command;
 
 import KonzoleVeci.Hra;
+import KonzoleVeci.Predmet;
 
 public class Prozkoumej implements Command {
 
     @Override
     public String vykonat(Hra hra, String s) {
-    return null;
+        String vypis = "";
+        boolean dobryDenPaniUcitelko = true;
+        boolean neco = false;
+
+        for (Predmet p : hra.getAktualniMistnost().getPredmety()) {
+            if (p.isSkryty()) {
+                p.setSkryty(false);
+                neco = true;
+                if (!dobryDenPaniUcitelko) {
+                    vypis += "";
+                }
+                vypis += p.getNazev();
+                dobryDenPaniUcitelko = false;
+            }
+        }
+        if (!neco) {
+            return "Nic noveho jsi neobjevil.";
+        }
+        return "Odhalil jsi: " + vypis;
+
     }
 }
