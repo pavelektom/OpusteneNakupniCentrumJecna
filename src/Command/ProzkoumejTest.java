d1c787b732f3ddd73cd4283a1455cc67bccdfb0c
tompa <tompavelek23@gmail.com>
2026-02-12 12:41:16 +0100

    dalsi novy test

---
+++ src/Command/ProzkoumejTest.java
@@ -0,0 +1,30 @@
+package Command;
+
+import KonzoleVeci.Hra;
+import KonzoleVeci.Mistnost;
+import KonzoleVeci.Predmet;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProzkoumejTest {
+    Hra hra = new Hra();
+    Prozkoumej prozkoumej = new Prozkoumej();
+
+    @Test
+    void odhalenoJupi() {
+        hra.getNacitani().mistnosti.clear();
+        hra.getNacitani().mistnosti.add(new Mistnost("hala", "velká hala", false, false, true));
+        hra.getNacitani().mistnosti.getFirst().getPredmety().add(new Predmet("em", "ahoj", true));
+        hra.setAktualniMistnost(hra.getNacitani().mistnosti.getFirst());
+        assertEquals("Odhalil jsi: em", prozkoumej.vykonat(hra, ""));
+    }
+    @Test
+    void prazdnaMistnost() {
+        hra.getNacitani().mistnosti.clear();
+        hra.getNacitani().mistnosti.add(new Mistnost("hala", "velká hala", false, false, true));
+        hra.setAktualniMistnost(hra.getNacitani().mistnosti.getFirst());
+        assertEquals("Nic noveho jsi neobjevil.", prozkoumej.vykonat(hra, ""));
+    }
+
+}
